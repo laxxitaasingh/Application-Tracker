9ac13a4b0a7e61dc277967e081cb3e871971c349
laxita-singh-robomq <laxita.singh@robomq.io>
2025-04-19 18:01:56 +0530

    created basic application page

--- frontend/src/app/applications/applications.component.ts
+++ frontend/src/app/applications/applications.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrl: './applications.component.css'
 })
 export class ApplicationsComponent {
+  applications: any[] = [];
+  newApp = { company: '', position: '', status: 'Applied' };
+  statuses = ['Applied', 'Interview', 'Rejected', 'Offered', 'On Hold'];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchApplications();
+  }
+
+  fetchApplications() {
+    const token = localStorage.getItem('token');
+    this.http.get<any[]>('http://localhost:3000/applications',{
+      headers: new HttpHeaders({
+        Authorization: `${token}`
+      })
+    }).subscribe(data => this.applications = data);
+  }
+  addApplication() {
+    const token = localStorage.getItem('token');
+  
+    this.http.post('http://localhost:3000/applications', this.newApp, {
+      headers: new HttpHeaders({
+        Authorization: `${token}`
+      })
+    }).subscribe(() => {
+      this.newApp = { company: '', position: '', status: 'Applied' };
+      this.fetchApplications();
+    });
+  }
+  deleteApplication(id: number) {
+    const token = localStorage.getItem('token');
+    this.http.delete(`http://localhost:3000/applications/${id}`,{
+      headers: new HttpHeaders({
+        Authorization: `${token}`
+      })
+    }).subscribe(() => this.fetchApplications());
+  }
 }
