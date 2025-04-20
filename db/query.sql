82cb03bb37954d179454a64a27121220a07575b5
laxita-singh-robomq <laxita.singh@robomq.io>
2025-04-20 11:49:48 +0530

    db query

---
+++ db/query.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `applications` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `company` varchar(255) DEFAULT NULL,
+  `position` varchar(255) DEFAULT NULL,
+  `status` enum('Applied','Interview','Rejected','Offered','On Hold') DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_id` (`user_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
